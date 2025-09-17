3183835c0608232d956b46a30cf02c2c369e8d01
Jim Fowler <fowler@math.osu.edu>
2025-09-17 19:44:46 -0400

    adding certs

--- shell.nix
+++ shell.nix
@@ -5,6 +5,8 @@ pkgs.mkShell {
 
   packages = [
     pkgs.curl
+    pkgs.cacert
+
     (pkgs.python312.withPackages (ps: with ps; [
       beautifulsoup4
       lxml           # parser backend for BeautifulSoup (fast & robust)
@@ -12,9 +14,15 @@ pkgs.mkShell {
     ]))
   ];
 
+  # Make TLS certs visible inside the shell
+  SSL_CERT_FILE     = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+  NIX_SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+
   # (Optional) keep user site-packages from leaking in
   shellHook = ''
     export PYTHONNOUSERSITE=1
+    export CURL_CA_BUNDLE="$SSL_CERT_FILE"
+    export GIT_SSL_CAINFO="$SSL_CERT_FILE"
   '';
 }
 
